f3fa374ee8761092799d38156cc03751b6d608b3
Nurudeen Sulaimon <nerodean@Neros-MacBook-Pro.local>
2023-07-20 04:36:56 -0400

    Iteration 2 completed

--- lib/vehicles.rb
+++ lib/vehicles.rb
@@ -1,10 +1,28 @@
+require './lib/passengers'
 class Vehicle 
-attr_reader :year, :make, :model
-  def initialize(year,make,model)
+  @vehicle
+attr_reader :year, :make, :model, :speeding, :passengers
+  def initialize(year,make,model,speeding,passengers)
     @year = year 
     @make = make 
     @model= model
+    @speeding=false
+    @passengers=[]
+    
   end
-end 
 
-  
+  def speeding?
+    @speeding
+  end 
+
+  def speed 
+    @speeding=true
+  end 
+
+   def add_passengers(passenger)
+       @passengers << passenger
+   end   
+   def num_adults
+       passengers=2
+   end 
+end 
--- spec/vehicles_spec.rb
+++ spec/vehicles_spec.rb
@@ -3,30 +3,52 @@ require './lib/passengers'
 require 'pry'
 RSpec.describe Vehicle do 
     it "exists" do
-      vehicle = Vehicle.new("2001", "Honda", "Civic")
+      vehicle = Vehicle.new("2001", "Honda", "Civic","speeding","passengers")
       expect(vehicle).to be_a Vehicle
     end 
 
     it 'has year' do 
-      vehicle = Vehicle.new("2001", "Honda", "Civic")
+      vehicle = Vehicle.new("2001", "Honda", "Civic","speeding","passengers")
       expect(vehicle.year).to eq("2001")
     end 
    
     it 'has make' do 
-      vehicle = Vehicle.new("2001", "Honda", "Civic")
+      vehicle = Vehicle.new("2001", "Honda", "Civic","speeding","passengers")
       expect(vehicle.make).to eq("Honda")
     end 
     
     it "has a model" do
-      vehicle = Vehicle.new("2001", "Honda", "Civic")
+      vehicle = Vehicle.new("2001", "Honda", "Civic","speeding","passengers")
       expect(vehicle.model).to eq("Civic")
     end 
 
-    xit "has a method speeding" do 
-      vehicle = Vehicle.new("2001", "Honda", "Civic")
+    it "has a method speeding" do 
+      vehicle = Vehicle.new("2001", "Honda", "Civic","speeding","passengers")
        expect(vehicle.speeding?).to eq(false)
        vehicle.speed
        expect(vehicle.speeding?).to eq(true)
     end 
+ 
+    describe Vehicle do
+    it  "adds passenger to the vehicle" do
+      vehicle = Vehicle.new("2001", "Honda", "Civic","speeding","passengers")
+      #vehicle=Vehicle.new
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})  
+      jude = Passenger.new({"name" => "Jude", "age" => 20}) 
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12})  
+      vehicle.add_passengers(charlie)
+      vehicle.add_passengers(jude)
+      vehicle.add_passengers(taylor)   
+      binding.pry  
+      expect(vehicle.passengers).to eq([charlie,jude, taylor])
+    end 
+  end 
+      it "has a method vehicle.num_adults" do
+      #vehicle=Vehicle.new("2001", "Honda", "Civic","speeding","passengers")
+      vehicle.num_adults
+      expect(vehicle.num_adults).to eq(2)
+      end
 end 
+   
 
+   
